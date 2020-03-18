68b7144e142bbba1d65fc8f6e0002993eb9a7028
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-03-18 11:15:11 +0100

    unable to fix error handling

--- group/GroupController.js
+++ group/GroupController.js
@@ -21,7 +21,7 @@ router.post('/register', VerifyToken, function(req, res) {
 
     Group.create({
         name : req.body.name,
-        description : req.body.desc,
+        description : req.body.description,
         admin : JSON.stringify([req.userId])
       }, 
 
@@ -33,10 +33,44 @@ router.post('/register', VerifyToken, function(req, res) {
     
 });
 
+
+
+router.put('/:id',  VerifyToken, function (req, res) {
+    
+    Group.findById(req.params.id, function (err, group) {
+        if (err) return res.status(500).send("There was a problem finding the group.");
+        if (!group) return res.status(404).send("No group found.");
+        if (!(JSON.parse([group.admin]).includes(req.userId))) return res.status(401).send("You have no authorization");
+
+        var cur_admin = JSON.parse([group.admin]);
+        var new_admin = JSON.parse(req.body.admin);
+        if(new_admin.length != 0){
+        for (i = 0; i< new_admin.length; i++){
+            User.findOne({ _id: new_admin[i] }, function (err, user) {
+                if (err) return res.status(500).send('Error on the server.');
+                if (!user) return res.status(404).send(`No user ${new_admin[i]} found.`);
+            });   
+        }
+        console.log("hererererr");
+        var total_admin = [...new Set([...cur_admin, ...new_admin])];
+        req.body.admin = JSON.stringify(total_admin);
+    }
+        Group.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, group) {
+            if (err) return res.status(500).send("There was a problem updating the group.");
+            res.status(200).send(group);
+        });
+});
+});
+
+router.post('/:id',  VerifyToken, function (req, res) {
+    
+    Group.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, group) {
+        if (err) return res.status(500).send("There was a problem updating the group.");
+        res.status(200).send(group);
+    });
+});
+
 router.delete('/:id', VerifyToken, function (req, res) {
-
-
-
     Group.findById(req.params.id, function (err, group) {
         if (err) return res.status(500).send("There was a problem finding the group.");
         if (!group) return res.status(404).send("No group found.");
@@ -48,7 +82,6 @@ router.delete('/:id', VerifyToken, function (req, res) {
             if (err) return res.status(500).send("There was a problem deleting the group.");
             res.status(200).send("group: "+ group.name +" was deleted.");
         });
-
 });
 });
 
