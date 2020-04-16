16ba8218b3fb2f97d7d7ed454ca1ea3806acbed1
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-04-16 21:41:22 +0100

    Sent Verification mail and password change mail successfully

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 var VerifyToken = require('./VerifyToken');
+var VerifyTokenExt = require('./VerifyTokenExt');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -14,6 +15,34 @@ var User = require('../user/User');
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var bcrypt = require('bcryptjs');
 var config = require('../config'); // get config file
+
+
+function send_mail(x){
+  const transporter = nodemailer.createTransport({
+    port: 25,
+    host: 'localhost',
+    tls: {
+      rejectUnauthorized: false
+    },
+  });
+
+  var message = {
+    from: 'noreply@halqah.com',
+    to: 'whatever@localhost.com',
+    subject: 'Confirm Email',
+    text: 'Please confirm your email',
+    html: `<p>Please confirm your email with ${x}</p>`
+  };
+
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
+}
+
+
 
 router.post('/login', function(req, res) {
 
@@ -59,7 +88,9 @@ router.post('/register', function(req, res) {
       expiresIn: 900 // expires in 15 minutes
     });
     //send email
-    res.status(200).send({ auth: true, token: token });
+    var link = `http://localhost:3000/api/auth/verify/${token}`;
+    send_mail(`<div><h2><b> Verify your Account </b></h2> <hr></br>	<p>You attempted to Create an Halqoh account.</br>Click <a href="${link}"><input type="button" value="Here"></a> to verify your email.<hr>If you did not attempt to create an Halqoh account, kindly ignore. <br>Thank you.</p></div>`);
+    res.status(200).send("Verification link Sent to Email");
   });
 
 });
@@ -74,7 +105,7 @@ router.get('/me', VerifyToken, function(req, res, next) {
 
 });
 
-router.get('/verify', VerifyToken, function(req, res) {
+router.get('/verify/:key', VerifyTokenExt, function(req, res) {
   console.log("req.userId");
   User.findByIdAndUpdate(req.userId, {verified : true}, {new: true}, function (err, user) {
     if (err) return res.status(500).send("Invalid Request.");
@@ -99,13 +130,15 @@ router.get('/forget-password/:email', function(req, res) {
 
     // return the information including token as JSON
     //send_email_template({ auth: true, token: token })
+    var link = `http://localhost:3000/api/auth/ret-pass/${token}`;
+    send_mail(`<div><h2><b> Reset Your Password </b></h2>	<p>You attempted to change the Password for your Halqoh account.Click <a href="${link}"><input type="button" value="Here"></a> and You will be redirected to a page where you can change your Password.<hr>If you did not request to change your Password, kindly ignore. <br>Thank you.</p></div>`);
     res.status(200).send("Sent to Email");
     // res.status(200).send({ auth: true, token: token });
   });
 
 });
 
-router.get('/ret-pass', VerifyToken, function(req, res) {
+router.get('/ret-pass/:key', VerifyTokenExt, function(req, res) {
   console.log("redirecting");
   //redirect and probably make new token (short one)
   res.status(200).send("redirecting to password page");
@@ -120,7 +153,7 @@ router.get('/:id', function (req, res) {
   });
 });
 
-router.put('/ret-pass', VerifyToken, function (req, res) {
+router.put('/ret-pass/:key', VerifyTokenExt, function (req, res) {
 
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
@@ -133,6 +166,8 @@ router.put('/ret-pass', VerifyToken, function (req, res) {
 
 
 });
+
+
 
 
 module.exports = router;
