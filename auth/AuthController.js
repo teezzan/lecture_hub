805780fdd4a8a5d64f8c5f922127e1538823a87a
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-21 18:44:17 +0100

    masked sensitive info of user

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -282,6 +282,10 @@ router.get('/me', cors(), VerifyToken, function (req, res, next) {
   User.findById(req.userId, { password: 0 }, function (err, user) {
     if (err) return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
+    delete user.password;
+    delete user.resetPasswordExpires;
+    delete user.resetPasswordToken;
+
     res.status(200).send(user);
   });
 
