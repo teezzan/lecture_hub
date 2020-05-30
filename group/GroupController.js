122af6264ebd435fa2c751ec569a83120bde267e
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-30 02:31:50 +0100

    trying to fix crashes

--- group/GroupController.js
+++ group/GroupController.js
@@ -625,18 +625,18 @@ router.get("/info/media", cors(), (req, res) => {
 router.get("/:id/media/del/:media_id", cors(), VerifyToken, VerifyAdmin, (req, res) => {
 
 
-    Group.findByIdAndUpdate(req.params.id, {$pull: {media: {filename: req.params.media_id}}}, function (err, groups) {
-      if (err) return res.status(500).send("There was a problem updating the group.");
+  Group.findByIdAndUpdate(req.params.id, { $pull: { media: { filename: req.params.media_id } } }, function (err, groups) {
+    if (err) return res.status(500).send("There was a problem updating the group.");
 
 
-      gfs.delete(new mongoose.Types.ObjectId(req.params.media_id), (err, data) => {
-        if (err) return res.status(404).json({ err: err.message });
-        
+    gfs.delete(new mongoose.Types.ObjectId(req.params.media_id), (err, data) => {
+      if (err) return res.status(404).json({ err: err.message });
+      // res.redirect("/");
 
-        res.status(200).send("Success");
-      });
-
+      res.status(200).send(groups);
     });
+
+  });
 });
 
 
