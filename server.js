2316a99cb7bf1a1077bf4bef0e65c4cc27d5840f
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-06-01 05:19:07 +0100

    commented out wss part TBD

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
 
-const wss = new SocketServer({ server });
-//init Websocket ws and handle incoming connect requests
-wss.on('connection', function connection(ws) {
-    console.log("connection ...");
-    //on connect message
-    ws.on('message', (data) => {
+// const wss = new SocketServer({ server });
+// //init Websocket ws and handle incoming connect requests
+// wss.on('connection', function connection(ws) {
+//     console.log("connection ...");
+//     //on connect message
+//     ws.on('message', (data) => {
 
-      // sends the data to all connected clients
-      wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-          }
-      });
-    });
-    ws.send('message from server at: ' + new Date());
-});
+//       // sends the data to all connected clients
+//       wss.clients.forEach((client) => {
+//           if (client.readyState === WebSocket.OPEN) {
+//             client.send(data);
+//           }
+//       });
+//     });
+//     ws.send('message from server at: ' + new Date());
+// });
