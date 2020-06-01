0368c879c31957a452429b181661aa48e9ec3be3
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-06-01 05:09:07 +0100

    removed node_modules again

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 var app = require('./app');
 var port = process.env.PORT || 3000;
+const SocketServer = require('ws').Server;
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
+});
+
+const wss = new SocketServer({ server });
+//init Websocket ws and handle incoming connect requests
+wss.on('connection', function connection(ws) {
+    console.log("connection ...");
+    //on connect message
+    ws.on('message', (data) => {
+
+      // sends the data to all connected clients
+      wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+          }
+      });
+    });
+    ws.send('message from server at: ' + new Date());
 });
