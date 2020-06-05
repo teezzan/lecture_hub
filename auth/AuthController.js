ada7f303fc8cb4a9cb6f48f865299eb2fbcbf900
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-06-05 22:11:32 +0100

    testing username error

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -506,7 +506,7 @@ router.get('/finduser/:id', cors(), function (req, res) {
 // username
 router.post('/finduser', cors(), function (req, res) {
   User.find({$or :[{username: req.body.tag},{name: req.body.tag},{email: req.body.tag},{_id: req.body.tag}] }, { password: 0, resetPasswordExpires: 0, resetPasswordToken: 0 }, function (err, user) {
-    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (err) return res.status(500).send(err);
     if (!user) return res.status(404).send("No user found.");
     res.status(200).send(user);
   });
