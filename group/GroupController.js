ac08bace31cd69876e10c94c1cd5a08609185014
teezzan <teehazzan@gmail.com>
2020-08-12 06:26:04 +0100

    tryna fix header error

--- group/GroupController.js
+++ group/GroupController.js
@@ -662,7 +662,7 @@ gfs.openDownloadStreamByName(req.params.filename,
         console.log("entered last one file=> ", file);
 
 //        res.header('Content-Length', file.length);
-  //      res.header('Content-Type', file.contentType);
+        res.header('Content-Type', file.contentType);
 
 	gfs.openDownloadStreamByName(req.params.filename).pipe(res);
 
